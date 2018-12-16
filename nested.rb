6ab0b022fff5bd41b69aa6656889899fc6bb5ac1
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-16 21:07:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,10 @@ def adding_matz
         }
      }
      
-  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-  programmer_hash[:yukihiro_matsumoto][:known_for] = 'Ruby'
+  new_hash = programmer_hash[:yukihiro_matsumoto][:known_for]
+      
+  puts new_hash
+  
     
 end
 

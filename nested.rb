24737b2a1b896e4e8baf48e7df6bfc90295b74ac
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-16 21:12:02 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,6 +110,12 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+
+    
+  programmer_hash[:alan_kay][:known_for] = alans_new_info
+  programmer_hash
+
+     
      
 end
 
@@ -130,9 +136,6 @@ def adding_to_dennis
         }
      }
   
-  new_info = "GUI" 
-    
-  programmer_hash[:alan_kay][:known_for] = new_info
+  programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
   programmer_hash
-
 end
